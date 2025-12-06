ec5fcfdcb191e3e4f08d052eeda60b9831288f0e
千夜 <1538548527@qq.com>
2025-12-07 06:38:42 +0800

    Delete 项目基础架构 directory

--- 项目基础架构/obj/local/arm64-v8a/objs/QianyeKernel/src/offset_scanner.o.d
+++
@@ -1,3 +0,0 @@
-./obj/local/arm64-v8a/objs/QianyeKernel/src/offset_scanner.o: \
-  jni/src/offset_scanner.cpp jni/include/driver.h
-jni/include/driver.h:
--- 项目基础架构/obj/local/arm64-v8a/objs/QianyeKernel/src/wzry_reader.o.d
+++
@@ -1,3 +0,0 @@
-./obj/local/arm64-v8a/objs/QianyeKernel/src/wzry_reader.o: \
-  jni/src/wzry_reader.cpp jni/include/driver.h
-jni/include/driver.h:
